9cdc7640cb285272b3b5559a271b185b09ac8fa9
Dave Doolin <david.doolin@gmail.com>
2023-11-13 05:04:12 -0800

    more work on n+1

--- demo.rb
+++ demo.rb
@@ -1,8 +1,13 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'active_record'
 require 'sqlite3'
 
 # Database Setup
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+# ActiveRecord::Base.logger.level = Logger::INFO  # Or another level, like Logger::WARN
 
 # Define Schema
 ActiveRecord::Schema.define do
@@ -16,23 +21,28 @@ ActiveRecord::Schema.define do
   end
 end
 
-# Define Models
-class Post < ActiveRecord::Base
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end
+
+# Define post model
+class Post < ApplicationRecord
   has_many :comments
 end
 
-class Comment < ActiveRecord::Base
+# Define comment model
+class Comment < ApplicationRecord
   belongs_to :post
 end
 
 # Seeding Data
-post = Post.create(title: "Example Post")
-post.comments.create(body: "This is a comment")
-post.comments.create(body: "This is another comment")
+post = Post.create(title: 'Example Post')
+post.comments.create(body: 'This is a comment')
+post.comments.create(body: 'This is another comment')
 
 # Querying Data
 begin
-  post_id = post.id  # Use the ID of the post you want to retrieve
+  post_id = post.id # Use the ID of the post you want to retrieve
 
   # Efficiently retrieving a post and its comments
   queried_post = Post.includes(:comments).find(post_id)
@@ -44,4 +54,3 @@ begin
 rescue ActiveRecord::RecordNotFound
   puts "Post with id #{post_id} not found."
 end
-
