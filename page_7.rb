3a547fa9bc2857187307d30982e189dceb627cdf
Dave Doolin <david.doolin@gmail.com>
2024-02-14 05:16:38 -0800

    n+1: page 7

--- page_7.rb
+++ page_7.rb
@@ -8,24 +8,29 @@ require 'query_count'
 
 require_relative 'setup'
 
-# Define post model
-class Post < ApplicationRecord
-  has_many :comments
+# Page 7 solves the prpblem by sorting in Ruby.
+module Page7
+  # Define post model
+  class Post < ApplicationRecord
+    has_many :comments
 
-  def latest_comment
-    # sort_by will sort in ruby, uses memory instead of database calls.
-    comments.sort_by(&:created_at).last
+    def latest_comment
+      # sort_by will sort in ruby, uses memory instead of database calls.
+      # comments.sort_by(&:created_at).last # rubocop:disable Style/RedundantSort
+      # rubocop suggests
+      comments.max(&:created_at)
+    end
   end
-end
 
-# Define comment model
-class Comment < ApplicationRecord
-  belongs_to :post
+  # Define comment model
+  class Comment < ApplicationRecord
+    belongs_to :post
+  end
 end
 
 def seed(post_count:, comment_count:)
   (1..post_count).each do |i|
-    post = Post.create(title: "Example Post #{i}")
+    post = Page7::Post.create(title: "Example Post #{i}")
     (1..comment_count).each do |j|
       post.comments.create(body: "Comment #{j}")
     end
@@ -35,6 +40,6 @@ end
 # Page 7, sorting in Ruby
 seed(post_count: 1, comment_count: 1)
 ActiveRecord::Base.logger = Logger.new($stdout)
-Post.includes(:comments).find_each do |post|
+Page7::Post.includes(:comments).find_each do |post|
   puts post.latest_comment
 end
