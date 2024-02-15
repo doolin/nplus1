a602612936f8d73d0f8bdd371ae02d81721c01f3
Dave Doolin <david.doolin@gmail.com>
2024-02-15 03:53:08 -0800

    n+1: page 8 verified

--- page_8.rb
+++ page_8.rb
@@ -8,18 +8,11 @@ require 'query_count'
 
 require_relative 'setup'
 
-# Page 7 solves the prpblem by sorting in Ruby.
-module Page7
+# Page 8 use default comment order.
+module Page8
   # Define post model
   class Post < ApplicationRecord
-    has_many :comments
-
-    def latest_comment
-      # sort_by will sort in ruby, uses memory instead of database calls.
-      comments.sort_by(&:created_at).last # rubocop:disable Style/RedundantSort
-      # rubocop suggests, which crashes
-      # comments.max(&:created_at)
-    end
+    has_many :comments, -> { order(created_at: :desc) }
   end
 
   # Define comment model
@@ -30,7 +23,7 @@ end
 
 def seed(post_count:, comment_count:)
   (1..post_count).each do |i|
-    post = Page7::Post.create(title: "Example Post #{i}")
+    post = Page8::Post.create(title: "Example Post #{i}")
     (1..comment_count).each do |j|
       post.comments.create(body: "Comment #{j}")
     end
@@ -40,6 +33,6 @@ end
 # Page 7, sorting in Ruby
 seed(post_count: 1, comment_count: 5)
 ActiveRecord::Base.logger = Logger.new($stdout)
-Page7::Post.includes(:comments).find_each do |post|
-  puts post.latest_comment
+Page8::Post.includes(:comments).find_each do |post|
+  puts post.comments.last
 end
