df4a8a4a27473f4edcc2438b0bc3ca93c1ae01f2
dave doolin <david.doolin@gmail.com>
2024-02-22 04:20:46 -0800

    prosopite_demo configured to run

--- prosopite_demo/config/environments/production.rb
+++ prosopite_demo/config/environments/production.rb
@@ -94,4 +94,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.after_initialize do
+    Prosopite.rails_logger = true
+    Prosopite.raise = true
+  end
 end
