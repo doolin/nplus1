75dc352a42696655401af43122fd44316a83c681
dave doolin <david.doolin@gmail.com>
2024-02-23 04:02:26 -0800

    chapter2 base file

---
+++ chapter2.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'bundler/inline'
+
+# From https://greg.molnar.io/blog/a-single-file-rails-application/
+# Necessary for single file Rails application, not sure how I want
+# to handle this yet.
+gemfile(true) do
+  source 'https://rubygems.org'
+
+  gem 'rails'
+  gem 'sqlite3'
+  gem 'colorize'
+  gem 'pg_query'
+end
+
+require 'active_record'
+require 'logger'
+require 'sqlite3'
+require 'colorize'
+
+require_relative 'setup'
+
+# Define post model
+class User < ApplicationRecord
+  # self.strict_loading_by_default = true
+
+  has_many :comments
+end
+
+# Define comment model
+class Comment < ApplicationRecord
+  belongs_to :user
+end
+
+def seed(user_count:, comment_count:)
+  (1..user_count).each do |i|
+    user = User.create(first_name: "Name#{i}")
+    (1..comment_count).each do |j|
+      user.comments.create(body: "Comment #{j}")
+    end
+  end
+end
+
+banner = <<~BANNER
+  Strict loading is a feature that helps you to avoid N+1 queries by
+  raising an error when you try to load an association that was not
+  preloaded. This feature is useful to ensure that you are not
+  accidentally loading associations in a loop or in a view.
+
+  In this case, strict loading is being set on the model instead of
+  the association. This means that the error will be raised when you
+  try to load the association from any instance of the model.
+BANNER
+puts banner.yellow
+
+seed(user_count: 2, comment_count: 5)
+ActiveRecord::Base.logger = Logger.new($stdout)
+
+# Page 12. Strict loading on a relation.
+# user = User.first
+# begin
+#   user.comments.to_a
+# rescue ActiveRecord::StrictLoadingViolationError => e
+#   puts e.message.red
+# end
+
+user = User.first
+user.comments.to_a
